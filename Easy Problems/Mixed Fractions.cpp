08c9fec2936c1e6b665ff66ee16eaf25bbea9af4
Niloy Rahman <havocisgod@gmail.com>
2019-09-09 22:17:51 +0600

    Create Mixed Fractions.cpp

---
+++ Easy Problems/Mixed Fractions.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main (int argc , char** argv){
+
+    int n,d;
+    cin>>n>>d;
+    int c = n/d;
+    int nn = n%d;
+cout<<c<<" "<<nn<<" "<<d;
+
+    return 0;
+}
